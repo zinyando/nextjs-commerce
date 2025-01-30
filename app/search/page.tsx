174a2d44325d90641545d0c23d7ce8627f2ab58a
Lennex Zinyando <brizdigital@gmail.com>
2025-01-30 19:16:22 +0200

    WIP product rag search

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,7 +1,8 @@
 import Grid from 'components/grid';
 import ProductGridItems from 'components/layout/product-grid-items';
 import { defaultSort, sorting } from 'lib/constants';
-import { getProducts } from 'lib/shopify';
+import { searchProducts } from 'lib/mastra/pgvector';
+// import { getProducts } from 'lib/shopify';
 
 export const metadata = {
   title: 'Search',
@@ -15,7 +16,9 @@ export default async function SearchPage(props: {
   const { sort, q: searchValue } = searchParams as { [key: string]: string };
   const { sortKey, reverse } = sorting.find((item) => item.slug === sort) || defaultSort;
 
-  const products = await getProducts({ sortKey, reverse, query: searchValue });
+  // const products = await getProducts({ sortKey, reverse, query: searchValue });
+  const products = await searchProducts(searchValue || '');
+  console.log(products);
   const resultsText = products.length > 1 ? 'results' : 'result';
 
   return (
